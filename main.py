55cb0fdeb52fee114af74ebf978e7d030ef1b1c3
Jan Szewczyk <36161455+QueIvan@users.noreply.github.com>
2022-06-05 15:56:43 +0200

    [Struct] Change from FOP to OOP

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import ring
+from ring import RingController
 
 
 def main():
+    ring_controller = RingController(24)
     while True:
-        ring.trail_wheel((0, 255, 0), .15)
+        ring_controller.trail_wheel((255, 0, 0), .15)
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('\n\n[SIGINT] Program terminated by user.')
-        ring.clear()
+    main()
--- ring.py
+++ ring.py
@@ -4,91 +4,80 @@ import board
 import neopixel
 import numpy as np
 
-num_pixels = 24
-pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.05, auto_write=False, pixel_order=neopixel.GRB)
-start_id = 0
-number = 0
 
+class RingController:
 
-# ********************
-#
-#  Ready functions
-#
-# ********************
+    def __init__(self, num_pixels):
+        self.__num_pixels = num_pixels
+        self.__pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.05, auto_write=False, pixel_order=neopixel.GRB)
+        self.__start_id = 0
+        self.__number = 0
 
-def set_pixel(index, color):
-    pixels[index] = color if index != 6 else (color[1], color[0], color[2])
-    pixels.show()
+    def __del__(self):
+        self.__pixels.fill((0, 0, 0))
+        self.__pixels.show()
 
+    # ********************
+    #
+    #  Ready functions
+    #
+    # ********************
 
-def step_fade_out(color, percent, step):
-    color = np.array(color)
-    target = np.array([0, 0, 0])
-    vector = target - color
-    result = color + vector * percent * step if min(color + vector * percent * step) >= 0 else (0, 0, 0)
-    return tuple(np.array(np.rint(result), dtype=int))
+    def set_pixel(self, index, color):
+        self.__pixels[index] = color if index != 6 else (color[1], color[0], color[2])
+        self.__pixels.show()
 
+    @staticmethod
+    def step_fade_out(color, percent, step):
+        color = np.array(color)
+        target = np.array([0, 0, 0])
+        vector = target - color
+        result = color + vector * percent * step if min(color + vector * percent * step) >= 0 else (0, 0, 0)
+        return tuple(np.array(np.rint(result), dtype=int))
 
-def create_trail(color, multiplier, ids):
-    if len(ids) > 0:
-        for j in ids:
-            step_count = ids.index(j) if min(ids) < num_pixels - (number + 1) else ids.index(j) + (number + 1) - len(
-                ids)
-            set_pixel(j, step_fade_out(color, multiplier, step_count))
-        pixels.show()
+    def create_trail(self, color, multiplier, ids):
+        if len(ids) > 0:
+            for j in ids:
+                step_count = ids.index(j) if min(ids) < self.__num_pixels - (self.__number + 1) else ids.index(j) + (
+                            self.__number + 1) - len(
+                    ids)
+                self.set_pixel(j, self.step_fade_out(color, multiplier, step_count))
+            self.__pixels.show()
 
+    def pixel_fill(self, color):
+        self.__pixels.fill(color if type(color) is tuple else (color, color, color))
+        self.__pixels[6] = (color[1], color[0], color[2]) if type(color) is tuple else (color, color, color)
+        self.__pixels.show()
 
-def pixel_fill(color):
-    pixels.fill(color if type(color) is tuple else (color, color, color))
-    pixels[6] = (color[1], color[0], color[2]) if type(color) is tuple else (color, color, color)
-    pixels.show()
-
-
-def set_brightness(brightness):
-    pixels.brightness = brightness / 100
-    pixels.show()
-    time.sleep(.05)
-
-
-def flash_brightness(high_value):
-    store_brightness = pixels.brightness * 100
-    set_brightness(high_value)
-    for i in range(0, high_value, 5):
-        set_brightness((high_value - i))
-    set_brightness(store_brightness)
-
-
-def trail_wheel(color, multiplier):
-    global number
-    number = math.ceil(max(color) / (max(color) * multiplier))
-    for i in range(start_id, num_pixels + number):
-        ids = get_index_seq(i)
-        create_trail(color, multiplier, ids)
+    def set_brightness(self, brightness):
+        self.__pixels.brightness = brightness / 100
+        self.__pixels.show()
         time.sleep(.05)
 
+    def flash_brightness(self, high_value):
+        store_brightness = self.__pixels.brightness * 100
+        self.set_brightness(high_value)
+        for i in range(0, high_value, 5):
+            self.set_brightness((high_value - i))
+        self.set_brightness(store_brightness)
 
-def get_index_seq(index):
-    global start_id
-    indexes = ()
-    for i in range(number + 1):
-        if 0 <= index - i < num_pixels:
-            indexes += (index - i,)
-        elif index - i >= num_pixels:
-            start_id = number
-            indexes += (index - i - num_pixels,)
-    return indexes
+    def trail_wheel(self, color, multiplier):
+        self.__number = math.ceil(max(color) / (max(color) * multiplier))
+        for i in range(self.__start_id, self.__num_pixels + self.__number):
+            ids = self.get_index_seq(i)
+            self.create_trail(color, multiplier, ids)
+            time.sleep(.05)
 
+    def get_index_seq(self, index):
+        indexes = ()
+        for i in range(self.__number + 1):
+            if 0 <= index - i < self.__num_pixels:
+                indexes += (index - i,)
+            elif index - i >= self.__num_pixels:
+                self.__start_id = self.__number
+                indexes += (index - i - self.__num_pixels,)
+        return indexes
 
-def clear():
-    pixels.fill((0, 0, 0))
-    pixels.show()
-
-
-# ********************
-#
-#  Dev Work
-#
-# ********************
-
-def test():
-    pixel_fill((0, 0, 0))
+    def clear(self):
+        self.__pixels.fill((0, 0, 0))
+        self.__pixels.show()
