3794ec501c368237cf867845221aa59663b9adc7
Jan Szewczyk <36161455+QueIvan@users.noreply.github.com>
2022-06-05 20:49:06 +0200

    [Struct] First package deployment test

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='one-of-eleven_board',
     version='0.1.0',
-    packages=find_packages(include=['one_of_eleven_board', 'one_of_eleven_board.*']),
+    packages=find_packages(include=['one-of-eleven_board', 'one-of-eleven_board.*']),
     install_requires=[
         'numpy',
         'adafruit-python-shell',
         'adafruit-blinka',
     ],
     entry_points={
-        'console_scripts': ['test_board=one_of_eleven_board.main:main']
+        'console_scripts': ['test_board=one-of-eleven_board.main:main']
     }
 )
